ddbe2b6f6e19ad2b12fbb2f68e1ca6fed0e435da
danek2077 <pprezident337@gmail.com>
2024-10-18 19:41:12 +0200

    i give up with this algorithm

--- src/pages/Event/ui/isAdminUsers/EventAdmin.tsx
+++ src/pages/Event/ui/isAdminUsers/EventAdmin.tsx
@@ -4,38 +4,78 @@ import styles from "./styles/evAdmin.module.scss";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../../redux-store/store";
 import { UsersData } from "../../../../redux-store/slices/firstSlice/types/TypesFirstSlice";
+type Event = {
+  event: string;
+};
+
+type UserEvents = {
+  [username: string]: Event[];
+};
+
+type DayData = {
+  day: number;
+  users: UserEvents[];
+};
+
+type DaysArray = DayData[];
+
 const EventAdmin = () => {
   const elems: JSX.Element[] = [];
   const users: UsersData[] = useSelector(
     (state: RootState) => state.firstSlice.readyUser
   );
-  const tasksArr: JSX.Element[] = [];
-  for (let i = 0; i < 7; ) {
-    users.map(function (el) {
-      const username = Object.keys(el);
-      //tasksArr
-      tasksArr.push(<nav key={Math.random()}></nav>);
-      let counterTask = 1;
-      let tasksDivs: JSX.Element[] = [];
-      Object.values(el).map(function (arr) {
-        arr.map(function (task) {
-          tasksDivs.push(
-            <div key={Math.random()}>
-              task {counterTask}: {task.event}
-            </div>
-          );
-          counterTask++;
+  const arr = [
+    {
+      day: 1,
+      users: [
+        {
+          dan: [
+            { event: "данечка в 3 день отдыхаешь" },
+            { event: "начать работать с 15:00" },
+          ],
+        },
+        {
+          nikita: [{ event: "работаешь в выходной, никитос" }],
+        },
+      ],
+    },
+  ];
+  const func = () => {
+    let rez: DaysArray = [];
+    for (let i = 1; i < 8; i++) {
+      rez.push({ day: i, users: [] });
+      users.map(function (el) {
+        const key = Object.keys(el)[0];
+        el[key].map(function (task) {
+          rez.map(function (rezEl) {
+            if (rezEl.day === task.day) {
+              if (rezEl.users[0] === undefined) {
+                let obj: UserEvents = {};
+
+                obj[key] = [];
+                rezEl.users.push(obj);
+              } else {
+                let flag = false;
+                rezEl.users.map(function (rezUser) {
+                  if (Object.keys(rezUser)[0] === key) {
+                    flag = true;
+                  }
+                });
+                if (flag) {
+                  let obj2: UserEvents = {};
+                  obj2[key] = [];
+                  
+                }
+              }
+            }
+          });
         });
       });
-      tasksArr[tasksArr.length - 1] = React.cloneElement(
-        tasksArr[tasksArr.length - 1],
-        {},
-        ...tasksDivs
-      );
-    });
-    i = 10;
-  }
 
+    }
+    console.log(rez);
+  };
+  func();
   // elems.push(
   //   <div key={Math.random()}>
   //     <span>{i} day</span>
@@ -51,7 +91,6 @@ const EventAdmin = () => {
   // );
   return (
     <div>
-      {tasksArr}
       {/* <div className={styles.flex}>
         <div key={Math.random()}>
           <span>1 day</span>
